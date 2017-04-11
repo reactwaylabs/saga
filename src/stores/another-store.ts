c689650669e70a07faa6a1c2f890dc987d391a52
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-11 11:22:15 +0300

    Hey

--- src/stores/another-store.ts
+++ src/stores/another-store.ts
@@ -2,6 +2,4 @@ import * as Flux from "flux";
 import { ReduceStore as FluxReduceStore } from "flux/utils";
 import * as Immutable from "immutable";
 import { Dispatcher, DispatcherMessage, DispatcherBuilder } from "../dispatcher";
-export type ActionHandler<TClass, TState> = (action: TClass, state: TState) => TState | void;
-export type StoreWillCleanup<TState> = () => void | TState;
 
