ebbec173a3c17f4f516d9c6f188118715debbf92
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-11 11:36:16 +0300

    Hey

--- src/stores/reduce-store.ts
+++ src/stores/reduce-store.ts
@@ -2,8 +2,10 @@ import * as Flux from "flux";
 import { ReduceStore as FluxReduceStore } from "flux/utils";
 import * as Immutable from "immutable";
 import { Dispatcher, DispatcherMessage, DispatcherBuilder } from "../dispatcher";
+
 export type ActionHandler<TClass, TState> = (action: TClass, state: TState) => TState | void;
 export type StoreWillCleanup<TState> = () => void | TState;
+
 export abstract class ReduceStore<TState> extends FluxReduceStore<TState, DispatcherMessage<any>> {
     /**
      * Creates an instance of ReduceStore.
