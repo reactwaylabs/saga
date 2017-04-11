df292ad87104edfde569cf86fd5a320155702947
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-11 11:22:58 +0300

    Hey

--- src/stores/another-store.ts
+++ src/stores/another-store.ts
@@ -4,4 +4,5 @@ import * as Immutable from "immutable";
 import { Dispatcher, DispatcherMessage, DispatcherBuilder } from "../dispatcher";
 
 export type ActionHandler<TClass, TState> = (action: TClass, state: TState) => TState | void;
+export type StoreWillCleanup<TState> = () => void | TState;
 
