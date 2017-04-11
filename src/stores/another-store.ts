44802476a79926e1570dbb07b73ec52165fc09fe
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-11 11:30:26 +0300

    Hey

--- src/stores/another-store.ts
+++ src/stores/another-store.ts
@@ -1,5 +1,5 @@
 import * as Flux from "flux";
-import { ReduceStore as FluxReduceStore } from "flux/utils";
+// import { ReduceStore as FluxReduceStore } from "flux/utils";
 import * as Immutable from "immutable";
 import { Dispatcher, DispatcherMessage, DispatcherBuilder } from "../dispatcher";
 
