734af20fa686a024388fa75511789bd9a6d12ac5
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-11 11:24:04 +0300

    Hey

--- src/stores/another-store.ts
+++ src/stores/another-store.ts
@@ -5,5 +5,4 @@ import { Dispatcher, DispatcherMessage, DispatcherBuilder } from "../dispatcher"
 
 export type ActionHandler<TClass, TState> = (action: TClass, state: TState) => TState | void;
 
-export type StoreWillCleanup<TState> = () => TState | void;
 
