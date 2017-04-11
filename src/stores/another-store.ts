9f1d13d32e580f2fd438bb3fa4eee3fb8194c3a7
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-11 11:29:50 +0300

    Hey

--- src/stores/another-store.ts
+++ src/stores/another-store.ts
@@ -3,7 +3,6 @@ import { ReduceStore as FluxReduceStore } from "flux/utils";
 import * as Immutable from "immutable";
 import { Dispatcher, DispatcherMessage, DispatcherBuilder } from "../dispatcher";
 
-export type ActionHandler<TClass, TState> = (action: TClass, state: TState) => TState | void;
 
 export abstract class ReduceStore<TState> extends FluxReduceStore<TState, DispatcherMessage<any>> {
 
