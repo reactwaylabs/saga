0e6bb9b1a486d551e4d975865486985890c9d73e
Giedrius Grabauskas <giedrius@quatrodev.com>
2017-09-26 15:12:27 +0300

    Fixed naming.

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,3 @@
 export { ItemIsPending } from "./helpers/item-is-pending";
 export { ItemsStatusResolver } from "./helpers/items-status-resolver";
-export { WaitForStoreChanges } from "./helpers/wait-for-store-changes";
+export { WaitForStoreChange } from "./helpers/wait-for-store-changes";
--- src/helpers/wait-for-store-change.ts
+++ src/helpers/wait-for-store-change.ts
@@ -1,6 +1,6 @@
 import { Store } from "flux/utils";
 
-export async function WaitForStoreChanges(store: Store<any>): Promise<void> {
+export async function WaitForStoreChange(store: Store<any>): Promise<void> {
     return new Promise<void>(resolve => {
         const listener = store.addListener(() => {
             listener.remove();
