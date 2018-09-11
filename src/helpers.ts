e77fafcd01c9f2b54160e8d725290cfc6d8cdcf4
Martynas Žilinskas <martynas@quatrodev.com>
2018-09-11 21:36:47 +0300

    Updated helpers entry point.

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,4 @@
-export { ItemIsPending } from "./helpers/item-is-pending";
-export { ItemsStatusResolver } from "./helpers/items-status-resolver";
-export { WaitForStoreChange } from "./helpers/wait-for-store-change";
+export * from "./helpers/item-is-pending";
+export * from "./helpers/items-status-resolver";
+export * from "./helpers/wait-for-store-change";
+export * from "./helpers/is-simplr-action";
