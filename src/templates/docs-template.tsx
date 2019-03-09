c54a143092a378eea029d5ee079b90d12ab5861d
Martynas Žilinskas <martiogalaltu@gmail.com>
2019-03-10 01:37:05 +0200

    Sidebar WIP.

--- src/templates/docs-template.tsx
+++ src/templates/docs-template.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { graphql } from "gatsby";
 
 import { Layout } from "../modules/layout";
+import { Sidebar } from "../modules/docs";
 
 export default function Template({
-    data, // this prop will be injected by the GraphQL query below.
+    data // this prop will be injected by the GraphQL query below.
 }) {
     const { markdownRemark } = data; // data.markdownRemark holds our post data
     const { html } = markdownRemark;
 
     return (
         <Layout>
+            <Sidebar />
             <div className="blog-post-container">
                 <div className="blog-post">
                     <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
