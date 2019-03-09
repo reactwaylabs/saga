8e45c8d8b590519eeafcc43b93406cde56b4613b
Martynas Žilinskas <martiogalaltu@gmail.com>
2019-03-09 14:03:03 +0200

    Initial gatsby project.

---
+++ gatsby-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    siteMetadata: {
+        title: "Yet Another State Management for JS Apps",
+        description:
+            "Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.",
+        author: "@reactway",
+    },
+    plugins: [
+        "gatsby-plugin-typescript",
+        "gatsby-plugin-react-helmet",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "${__dirname}/src/images",
+            },
+        },
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                name: "saga",
+                short_name: "saga",
+                start_url: "/",
+                background_color: "#663399",
+                theme_color: "#663399",
+                display: "minimal-ui",
+                icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+            },
+        }
+    ],
+};
