ee943837f395190c4317fd9e2718138d96dfe65d
Martynas Žilinskas <martiogalaltu@gmail.com>
2019-03-09 16:41:10 +0200

    WIP navigation.

--- src/modules/layout/navigation/navigation.tsx
+++ src/modules/layout/navigation/navigation.tsx
@@ -1,34 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
 
 import "./navigation.scss";
 
+function NavItem(props: { href: string; className?: string; children: React.ReactNode }): JSX.Element {
+    return (
+        <li className={props.className}>
+            <a href={props.href}>{props.children}</a>
+        </li>
+    );
+}
+
+function Nav(props: { className: string; children: React.ReactNode }): JSX.Element {
+    return (
+        <nav className={classNames("nav-list", props.className)}>
+            <ul className="menu">{props.children}</ul>
+        </nav>
+    );
+}
+
 export function Navigation(): JSX.Element {
+    const [mobileOpen, setMobileOpen] = useState(true);
+
     return (
         <div className="navigation">
-            <div className="wrapper">
-                <div className="logo">saga</div>
-                <nav>
-                    <ul className="menu">
-                        <li>
-                            <a href="#">One</a>
-                        </li>
-                        <li>
-                            <a href="#">Two</a>
-                        </li>
-                        <li>
-                            <a href="#">Three</a>
-                        </li>
-                        <li>
-                            <a href="#">Four</a>
-                        </li>
-                    </ul>
-                </nav>
-                <div className="more">
-                    <a href="#">v4.0.0-alpha</a>
-                    <a href="#" className="github">
-                        <i className="fab fa-github" />
-                    </a>
+            <div className={classNames("wrapper", { "mobile-open": mobileOpen })}>
+                <div className="logo">
+                    <span>saga</span>
+                    <span className="mobile-menu" onClick={() => setMobileOpen(!mobileOpen)}>
+                        <i className="fas fa-bars" />
+                    </span>
                 </div>
+                <Nav className="main-nav">
+                    <NavItem href="#">One</NavItem>
+                    <NavItem href="#">Two</NavItem>
+                    <NavItem href="#">Three</NavItem>
+                </Nav>
+                <Nav className="more">
+                    <NavItem href="#">v4.0.0-alpha</NavItem>
+                    <NavItem href="#" className="github">
+                        <i className="fab fa-github" />
+                    </NavItem>
+                </Nav>
             </div>
         </div>
     );
