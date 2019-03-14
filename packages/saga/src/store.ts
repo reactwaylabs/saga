a9f5ad46f52bc79de865f927052711139d484c84
Martynas Žilinskas <martynas@quatrodev.com>
2019-03-14 12:48:01 +0200

    Added Store helpers [WIP]

--- packages/saga/src/store.ts
+++ packages/saga/src/store.ts
@@ -1,6 +1,6 @@
 import { Dispatcher, DispatcherRegisterHandler } from "./dispatcher";
 import { TinyEmitter, Callback } from "./emitter";
-import { FSA } from "./actions";
+import { FSA, isSagaAction, ClassAction } from "./actions";
 
 class StoreClass<TState, TPayload extends FSA<any, any> = FSA<any, any>> implements Store<TState, TPayload> {
     constructor(
@@ -60,7 +60,7 @@ class StoreClass<TState, TPayload extends FSA<any, any> = FSA<any, any>> impleme
     };
 }
 
-export type StoreReduceHandler<TState, TPayload> = (state: TState, payload: TPayload) => TState;
+export type StoreReduceHandler<TState, TPayload extends FSA<any, any> = FSA<any, any>> = (state: TState, payload: TPayload) => TState;
 export type StoreAreEqualHandler<TState> = (state: TState, nextState: TState) => boolean;
 
 export interface Store<TState, TPayload extends FSA<any, any> = FSA<any, any>> {
@@ -88,3 +88,91 @@ export function createStore<TState, TPayload extends FSA<any, any> = FSA<any, an
 
     return new StoreClass<TState, TPayload>(options.initialState, options.dispatcher, options.reducer, areEqual);
 }
+
+export function registerActionHandler<TState, TAction extends ClassAction>(
+    action: TAction,
+    callback: (state: TState, action: InstanceType<TAction>) => TState
+): StoreReduceHandler<TState> {
+    return (state, payload) => {
+        if (isSagaAction(payload) && payload.payload instanceof action) {
+            return callback(state, payload.payload as InstanceType<TAction>);
+        }
+
+        return state;
+    };
+}
+
+export function combineHandlers<TState>(handlers: Array<StoreReduceHandler<TState, any>>): StoreReduceHandler<TState, any> {
+    return (state, payload) => {
+        let nextState = state;
+
+        for (const handler of handlers) {
+            nextState = handler(nextState, payload);
+        }
+
+        return nextState;
+    };
+}
+
+// prettier-ignore
+export type ActionHandler<TState, TActions extends FSA<any>> = {
+    [TType in TActions["type"]]: (state: TState, action: Extract<TActions, {type: TType}>) => TState
+};
+
+export function handleFluxActions<TState, TActions extends FSA<any>>(
+    handlers: ActionHandler<TState, TActions>
+): StoreReduceHandler<TState, TActions> {
+    return (state: TState, action: TActions): TState => {
+        for (const key in handlers) {
+            if (action.type === key) {
+                const handler = handlers[key as TActions["type"]];
+                return handler(state, action as any);
+            }
+        }
+        return state;
+    };
+}
+
+export interface IncrementAction
+    extends FSA<{
+        amount: string;
+    }> {
+    type: "INCREMENT";
+}
+
+export interface DecrementAction
+    extends FSA<{
+        amount: number;
+    }> {
+    type: "DECREMENT";
+}
+
+class IncrementClassAction {
+    public amount: number = 1;
+}
+
+type Actions = IncrementAction | DecrementAction;
+
+interface StoreState {
+    counter: number;
+}
+
+combineHandlers<StoreState>([
+    handleFluxActions<StoreState, Actions>({
+        INCREMENT: (state, action) => {
+            action.payload.amount;
+            return state;
+        },
+        DECREMENT: (state, action) => {
+            action.payload.amount;
+            return state;
+        }
+    }),
+    registerActionHandler(IncrementClassAction, (state, action) => {
+        action.amount;
+        return {
+            ...state,
+            counter: state.counter + action.amount
+        };
+    })
+]);
