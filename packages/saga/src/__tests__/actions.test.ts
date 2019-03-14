85699c386271b9b6bc906d609863b7e83a2711b0
Martynas Žilinskas <martynas@quatrodev.com>
2019-03-14 16:35:02 +0200

    Added tests for store action helpers.

--- packages/saga/src/__tests__/actions.test.ts
+++ packages/saga/src/__tests__/actions.test.ts
@@ -1,16 +1,24 @@
-import { createSagaAction } from "../actions";
+import { createSagaAction, createFluxAction, FSA } from "../actions";
 
 jest.mock("../helpers", () => ({
     ...jest.requireActual("../helpers"),
     generateRandomString: () => "RANDOM"
 }));
 
-class TestAction {}
+class TestSagaAction {}
+
+interface TestFSA extends FSA {
+    type: "TEST";
+}
 
 it("creates saga action from class payload", () => {
-    expect(createSagaAction(TestAction)).toMatchSnapshot();
+    expect(createSagaAction(TestSagaAction)).toMatchSnapshot();
 });
 
 it("throw when creating saga action from object payload", () => {
     expect(() => createSagaAction({})).toThrow();
+});
+
+it("creates FSA action", () => {
+    expect(createFluxAction<TestFSA>("TEST", undefined)).toMatchSnapshot();
 });
--- packages/saga/src/__tests__/store.test.ts
+++ packages/saga/src/__tests__/store.test.ts
@@ -1,18 +1,36 @@
-import { createStore, Store } from "../store";
+import { createStore, Store, combineHandlers } from "../store";
 import { createDispatcher, Dispatcher } from "../dispatcher";
-import { FSA, createFluxAction } from "../actions";
+import { FSA, createFluxAction, createSagaAction } from "../actions";
 
-import { handleFluxActions } from "../store";
+import { handleFluxActions, registerActionHandler } from "../store";
 
-interface ActionIncrement extends FSA<undefined> {
+interface IncrementAction extends FSA<{ plusCount: number }> {
     type: "COUNTER_INCREMENT";
 }
 
-interface ActionDecrement extends FSA<undefined> {
+interface DecrementAction extends FSA<{ minusCount: number }> {
     type: "COUNTER_DECREMENT";
 }
 
-type StoreActions = ActionIncrement | ActionDecrement;
+class IncrementClassAction {
+    constructor(private _plusCount: number = 1) {}
+
+    public get plusCount(): number {
+        return this._plusCount;
+    }
+}
+
+class DecrementClassAction {
+    constructor(private _minusCount: number = 1) {}
+
+    public get minusCount(): number {
+        return this._minusCount;
+    }
+}
+
+class ResetClassAction {}
+
+type StoreActions = IncrementAction | DecrementAction;
 
 let dispatcher = createDispatcher();
 let store = createTestStore(dispatcher);
@@ -71,7 +89,7 @@ it("dispatched action updates store state", () => {
     store.subscribe(stub);
     expect(store.getState().counter).toBe(0);
 
-    dispatcher.dispatchFSA(createFluxAction<ActionIncrement>("COUNTER_INCREMENT", undefined));
+    dispatcher.dispatchFSA(createFluxAction<IncrementAction>("COUNTER_INCREMENT", { plusCount: 1 }));
 
     expect(store.getState().counter).toBe(1);
     expect(store.hasChanged()).toBe(true);
@@ -84,9 +102,86 @@ it("dispatched action updates state and store emits change", () => {
     store.subscribe(stub);
     expect(store.getSubscribersCount()).toBe(1);
 
-    dispatcher.dispatchFSA(createFluxAction<ActionIncrement>("COUNTER_INCREMENT", undefined));
+    dispatcher.dispatchFSA(createFluxAction<IncrementAction>("COUNTER_INCREMENT", { plusCount: 1 }));
 
     expect(stub).toBeCalled();
     store.unsubscribe(stub);
     expect(store.getSubscribersCount()).toBe(0);
+});
+
+it("register action handler with class increment action", () => {
+    const handler = registerActionHandler<StoreState, typeof IncrementClassAction>(IncrementClassAction, (state, action) => {
+        return {
+            counter: state.counter + action.plusCount
+        };
+    });
+
+    let state: StoreState = {
+        counter: 0
+    };
+
+    state = handler(state, createSagaAction(new IncrementClassAction(2)));
+    expect(state.counter).toBe(2);
+});
+
+it("combined class action handlers", () => {
+    const handlers = combineHandlers<StoreState>([
+        registerActionHandler(IncrementClassAction, (state, action) => {
+            return {
+                counter: state.counter + action.plusCount
+            };
+        }),
+        registerActionHandler(DecrementClassAction, (state, action) => {
+            return {
+                counter: state.counter - action.minusCount
+            };
+        })
+    ]);
+
+    let state: StoreState = {
+        counter: 0
+    };
+
+    state = handlers(state, createSagaAction(new IncrementClassAction(2)));
+    expect(state.counter).toBe(2);
+    state = handlers(state, createSagaAction(new DecrementClassAction(1)));
+    expect(state.counter).toBe(1);
+});
+
+it("mixed class action handler and FSA handler", () => {
+    type Actions = IncrementAction | DecrementAction;
+
+    const handlers = combineHandlers<StoreState>([
+        handleFluxActions<StoreState, Actions>({
+            COUNTER_INCREMENT: (state, action) => {
+                return {
+                    ...state,
+                    counter: state.counter + action.payload.plusCount
+                };
+            },
+            COUNTER_DECREMENT: (state, action) => {
+                return {
+                    ...state,
+                    counter: state.counter - action.payload.minusCount
+                };
+            }
+        }),
+        registerActionHandler(ResetClassAction, (state, _action) => {
+            return {
+                ...state,
+                counter: 0
+            };
+        })
+    ]);
+
+    let state: StoreState = {
+        counter: 0
+    };
+
+    state = handlers(state, createFluxAction<IncrementAction>("COUNTER_INCREMENT", { plusCount: 2 }));
+    expect(state.counter).toBe(2);
+    state = handlers(state, createSagaAction(new ResetClassAction()));
+    expect(state.counter).toBe(0);
+    state = handlers(state, createFluxAction<DecrementAction>("COUNTER_DECREMENT", { minusCount: 2 }));
+    expect(state.counter).toBe(-2);
 });
