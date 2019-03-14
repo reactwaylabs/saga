85031594df7a4733b35f475380f5838327087ef7
Martynas Žilinskas <martynas@quatrodev.com>
2019-03-14 17:42:13 +0200

    Added more actions test coverage.

--- packages/saga/src/__tests__/actions.test.ts
+++ packages/saga/src/__tests__/actions.test.ts
@@ -1,4 +1,4 @@
-import { createSagaAction, createFluxAction, FSA } from "../actions";
+import { createSagaAction, createFluxAction, FSA, isFSA, isErrorFSA, isSagaAction } from "../actions";
 
 jest.mock("../helpers", () => ({
     ...jest.requireActual("../helpers"),
@@ -16,9 +16,44 @@ it("creates saga action from class payload", () => {
 });
 
 it("throw when creating saga action from object payload", () => {
-    expect(() => createSagaAction({})).toThrow();
+    const invalidClassAction = {};
+
+    expect(() => createSagaAction(invalidClassAction)).toThrow();
 });
 
 it("creates FSA action", () => {
-    expect(createFluxAction<TestFSA>("TEST", undefined)).toMatchSnapshot();
+    const fluxAction = createFluxAction<TestFSA>("TEST", undefined);
+
+    expect(fluxAction).toMatchSnapshot();
+});
+
+it("check if FSA object is FSA", () => {
+    const fluxAction = createFluxAction<TestFSA>("TEST", undefined);
+
+    expect(isFSA(fluxAction)).toBe(true);
+});
+
+it("check if FSA object is Error FSA", () => {
+    const actionPayload = new Error("Test Error");
+    const fluxAction = createFluxAction<TestFSA>("TEST", actionPayload);
+
+    expect(isErrorFSA(fluxAction)).toBe(true);
+});
+
+it("check if empty object is FSA", () => {
+    const action = {};
+
+    expect(isFSA(action)).toBe(false);
+});
+
+it("check if Saga Action is Saga action", () => {
+    const sagaAction = createSagaAction(new TestSagaAction());
+
+    expect(isSagaAction(sagaAction)).toBe(true);
+});
+
+it("check if object is Saga action", () => {
+    const invalidSagaAction = {};
+
+    expect(isSagaAction(invalidSagaAction)).toBe(false);
 });
