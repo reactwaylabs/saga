0d541a838cdbf56cf5fd3e9800c8e7a5806cfc4c
Martynas Žilinskas <martynas@quatrodev.com>
2019-03-28 17:12:45 +0200

    .

---
+++ examples/sample-project/src/modules/posts/posts-store.ts
@@ -0,0 +1,25 @@
+// import { createMapStore } from "saga-extra";
+
+// interface AwaitPromisesResult {
+//     success: 
+// }
+
+// function awaitAllPromises<TData = unknown>(promises: Array<Promise<TData>>): Promise<
+
+// export interface PostDto {
+//     id: number;
+//     userId: number;
+//     title: string;
+//     body: string;
+// }
+
+// export const PostsStore = createMapStore({
+//     name: "Posts",
+//     requestData: ids => {
+//         const response = fetch("https://jsonplaceholder.typicode.com/posts", {
+//             method: "GET"
+//         });
+
+
+//     }
+// });
